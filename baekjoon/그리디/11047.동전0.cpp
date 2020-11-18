02725701526466c880dcb715b2ecf5bbdd1f6d81
yunhalee05 <yunhalee0506@naver.com>
2020-11-18 20:16:32 +0900

    백준사이트 문제

---
+++ baekjoon/그리디/11047.동전0.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	vector<int> money;
+	int count = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		money.push_back(num);
+	}
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (k == 0)
+		{
+			break;
+		}
+		count += (k / money[i]);
+		k = (k % money[i]);
+	}
+
+	cout << count;
+
+
+	return 0;
+}
