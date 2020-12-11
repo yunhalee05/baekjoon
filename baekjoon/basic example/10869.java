9452df1fe88d1217f0f23945a8b24304947e7100
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2020-12-11 17:25:03 +0900

    Create 10869.java

---
+++ baekjoon/basic example/10869.java
@@ -0,0 +1,19 @@
+package Baekjoonexample;
+
+import java.util.*;
+public class Baekjoonexample {
+	public static void main(String arg[]) {
+		
+
+	
+	Scanner sc = new Scanner (System.in);
+	int A = sc.nextInt();
+	int B = sc.nextInt();
+	
+	System.out.println(A+B);
+	System.out.println(A-B);
+	System.out.println(A*B);
+	System.out.println(A/B);
+	System.out.println(A%B);
+	}
+}
